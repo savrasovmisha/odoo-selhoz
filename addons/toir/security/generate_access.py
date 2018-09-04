88a9de7b0770b197ffe9282cc3a1e4f70a876cb6
savrasov <savrasov@smv-pc.nofrr.local>
2018-09-04 13:30:18 +0500

    ТОиР

--- addons/toir/security/generate_access.py
+++ addons/toir/security/generate_access.py
@@ -1,17 +1,23 @@
 spisok = []
-spisok.append(['aktiv.categ',''])
-spisok.append(['aktiv.aktiv',''])
-spisok.append(['aktiv.type',''])
-spisok.append(['aktiv.status',''])
-spisok.append(['aktiv.vid_rabot',''])
-spisok.append(['aktiv.vid_rabot_price_line',''])
-spisok.append(['aktiv.vid_remonta',''])
-spisok.append(['aktiv.tr',''])
-spisok.append(['aktiv.tr_raboti_line',''])
-spisok.append(['aktiv.tr_nomen_line',''])
-spisok.append(['aktiv.gr',''])
-spisok.append(['aktiv.gr_line',''])
+spisok.append(['aktiv.categ','', 1])
+spisok.append(['aktiv.aktiv','', 1])
+spisok.append(['aktiv.type','', 1])
+spisok.append(['aktiv.status','', 1])
+spisok.append(['aktiv.vid_rabot','', 1])
+spisok.append(['aktiv.vid_rabot_price_line','', 1])
+spisok.append(['aktiv.vid_remonta','', 1])
+spisok.append(['aktiv.tr','', 1])
+spisok.append(['aktiv.tr_raboti_line','', 1])
+spisok.append(['aktiv.tr_nomen_line','', 1])
+spisok.append(['aktiv.gr','', 1])
+spisok.append(['aktiv.gr_line','', 1])
+spisok.append(['nomen.nomen','selhoz_base', 0])
+spisok.append(['nomen.nomen_price_line','selhoz_base', 0])
 
+
+# access_nomen_nomen_manager,nomen.nomen,selhoz_base.model_nomen_nomen,group_sklad_manager,1,1,1,1
+# access_nomen_nomen_price_line_manager,nomen.nomen_price_line,selhoz_base.model_nomen_nomen_price_line,group_sklad_manager,1,1,1,1
+#access_aktiv_gr_line_manager,aktiv.gr_line,model_aktiv_gr_line,group_toir_manager,1,1,1,1
 # spisok_read = []
 # spisok_read.append(['buh.nomen_group',''])
 # spisok_read.append(['buh.stati_zatrat',''])
@@ -37,7 +43,9 @@ for name in spisok:
 	if len(name[1])>0:
 		modul_name = name[1] + '.'
 	print 'access_%(class_name)s_manager,%(name)s,%(modul_name)smodel_%(class_name)s,group_toir_manager,1,1,1,1' % {'name':name[0], 'class_name': class_name, 'modul_name': modul_name}
-	print 'access_%(class_name)s_users,%(name)s,%(modul_name)smodel_%(class_name)s,group_toir_users,1,0,0,0'  % {'name':name[0], 'class_name': class_name, 'modul_name': modul_name}
+	
+	if name[2] == 1:
+		print 'access_%(class_name)s_users,%(name)s,%(modul_name)smodel_%(class_name)s,group_toir_users,1,0,0,0'  % {'name':name[0], 'class_name': class_name, 'modul_name': modul_name}
 
 # #REad only
 # for name in spisok_read:
